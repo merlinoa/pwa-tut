b2295cd023edebed85a5112f9502d90701f99286
merlinoa <merlinoa88@gmail.com>
2018-01-19 08:59:09 -0500

    enabled usb debugging of mobile device

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,5 +3,27 @@ if ('serviceWorker' in navigator) {
     .register('/sw.js')
     .then(() => {
       console.log('service worker registered!')  
+    }).catch(err => {
+      console.log(err)
     })  
 }
+
+// diable default mobile install prompt
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired')
+  event.preventDefault();
+  deferredPrompt = event
+  return false
+})
+
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve('This is executed once the timer is done')
+  }, 3000)
+});
+
+promise.then((text) => {
+  console.log(text)
+})
+
+console.log('This is executed right after setTimeout()')
